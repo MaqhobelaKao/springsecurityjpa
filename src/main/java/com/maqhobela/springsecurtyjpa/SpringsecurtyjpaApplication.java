bd59d0d6456a85d3a7a380f635697ddd33601652
MaqhobelaKao <kaomaqhobela@gmail.com>
2022-12-15 12:05:54 +0200

    initial spring application for using springsecurity on mysql using jpa

---
+++ src/main/java/com/maqhobela/springsecurtyjpa/SpringsecurtyjpaApplication.java
@@ -0,0 +1,13 @@
+package com.maqhobela.springsecurtyjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurtyjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurtyjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maqhobela/springsecurtyjpa/SpringsecurtyjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maqhobela.springsecurtyjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurtyjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
